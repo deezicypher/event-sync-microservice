3cefce627d2dca9574316e5355323033159b3d12
Deezi <deezitheviper@gmail.com>
2025-01-15 16:34:55 +0100

    update on posts

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Comments from "./Comments";
-import CreateComment from "./createcomment";
+import CreateComment from "./createComment";
 
 interface Post {
     id: string;
